60d9610022e73a4ec0959d9d7f35f0bb3d69d38b
DavidJulio97 <david.julio@mobilu.eu>
2024-09-08 19:35:18 +0100

    Get meals and render them on server side component

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,12 @@ import React from "react";
 import classes from "@/app/meals/page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function Meals() {
+// We can use async await in server side components
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +23,7 @@ export default function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
